7da4281c3bd889d7d869a4f9ae97d86b499d6470
William DeMeo <williamdemeo@gmail.com>
2024-06-15 10:19:10 -0600

    initial commit of MIL_wjd (copy of MIL)

---
+++ MIL_wjd/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
@@ -0,0 +1,192 @@
+import Mathlib.Algebra.Ring.Defs
+import Mathlib.Data.Real.Basic
+import MIL.Common
+
+section
+variable (R : Type*) [Ring R]
+
+#check (add_assoc : ∀ a b c : R, a + b + c = a + (b + c))
+#check (add_comm : ∀ a b : R, a + b = b + a)
+#check (zero_add : ∀ a : R, 0 + a = a)
+#check (add_left_neg : ∀ a : R, -a + a = 0)
+#check (mul_assoc : ∀ a b c : R, a * b * c = a * (b * c))
+#check (mul_one : ∀ a : R, a * 1 = a)
+#check (one_mul : ∀ a : R, 1 * a = a)
+#check (mul_add : ∀ a b c : R, a * (b + c) = a * b + a * c)
+#check (add_mul : ∀ a b c : R, (a + b) * c = a * c + b * c)
+
+end
+
+section
+variable (R : Type*) [CommRing R]
+variable (a b c d : R)
+
+example : c * b * a = b * (a * c) := by ring
+
+example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b := by ring
+
+example : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by ring
+
+example (hyp : c = d * a + b) (hyp' : b = a * d) : c = 2 * a * d := by
+  rw [hyp, hyp']
+  ring
+
+end
+
+namespace MyRing
+variable {R : Type*} [Ring R]
+
+theorem add_zero (a : R) : a + 0 = a := by rw [add_comm, zero_add]
+
+theorem add_right_neg (a : R) : a + -a = 0 := by rw [add_comm, add_left_neg]
+
+#check MyRing.add_zero
+#check add_zero
+
+end MyRing
+
+namespace MyRing
+variable {R : Type*} [Ring R]
+
+theorem neg_add_cancel_left (a b : R) : -a + (a + b) = b := by
+  rw [← add_assoc, add_left_neg, zero_add]
+
+-- Prove these:
+theorem add_neg_cancel_right (a b : R) : a + b + -b = a := by
+  rw [add_assoc, add_right_neg, add_zero]
+
+theorem add_left_cancel {a b c : R} (h : a + b = a + c) : b = c := by
+  rw [←neg_add_cancel_left a b, h, neg_add_cancel_left a c]
+
+theorem add_right_cancel {a b c : R} (h : a + b = c + b) : a = c := by
+  rw [←add_neg_cancel_right a b, h, add_neg_cancel_right c b]
+
+theorem mul_zero (a : R) : a * 0 = 0 := by
+  have h : a * 0 + a * 0 = a * 0 + 0 := by
+    rw [← mul_add, add_zero, add_zero]
+  rw [add_left_cancel h]
+
+theorem zero_mul (a : R) : 0 * a = 0 := by
+  have h : 0 * a + 0 * a = 0 * a + 0 := by
+    rw [← add_mul,add_zero, add_zero]
+  rw [add_left_cancel h]
+
+theorem neg_eq_of_add_eq_zero {a b : R} (h : a + b = 0) : -a = b := by
+  rw [←neg_add_cancel_left a b, h, add_zero]
+
+theorem eq_neg_of_add_eq_zero {a b : R} (h : a + b = 0) : a = -b := by
+  rw [←add_neg_cancel_right a b, h, zero_add]
+
+theorem neg_zero : (-0 : R) = 0 := by
+  apply neg_eq_of_add_eq_zero
+  rw [add_zero]
+
+theorem neg_neg (a : R) : - -a = a := by
+  rw [←eq_neg_of_add_eq_zero (add_right_neg a)]
+
+end MyRing
+
+-- Examples.
+section
+variable {R : Type*} [Ring R]
+
+example (a b : R) : a - b = a + -b :=
+  sub_eq_add_neg a b
+
+end
+
+example (a b : ℝ) : a - b = a + -b :=
+  rfl
+
+example (a b : ℝ) : a - b = a + -b := by
+  rfl
+
+namespace MyRing
+variable {R : Type*} [Ring R]
+
+theorem self_sub (a : R) : a - a = 0 := by
+  rw [sub_eq_add_neg, add_right_neg]
+
+theorem self_subℝ (a : ℝ) : a - a = 0 :=
+  by apply add_right_neg
+
+theorem self_subℝ' (a : ℝ) : a - a = 0 :=
+  by exact add_right_neg a
+
+theorem one_add_one_eq_two : 1 + 1 = (2 : R) := by
+  norm_num
+
+theorem two_mul (a : R) : 2 * a = a + a := by
+  have h : 2 * a = (1 + 1) * a := by rw [one_add_one_eq_two]
+  rw [h, add_mul, one_mul]
+
+theorem two_mul_calc (a : R) : 2 * a = a + a := by
+  calc
+    2 * a = (1 + 1) * a := by rw [one_add_one_eq_two]
+    _ = a + a := by rw [add_mul, one_mul]
+
+
+end MyRing
+
+section
+variable (A : Type*) [AddGroup A]
+
+#check (add_assoc : ∀ a b c : A, a + b + c = a + (b + c))
+#check (zero_add : ∀ a : A, 0 + a = a)
+#check (add_left_neg : ∀ a : A, -a + a = 0)
+
+end
+
+section
+variable {G : Type*} [Group G]
+
+#check (mul_assoc : ∀ a b c : G, a * b * c = a * (b * c))
+#check (one_mul : ∀ a : G, 1 * a = a)
+#check (mul_left_inv : ∀ a : G, a⁻¹ * a = 1)
+
+theorem idemp (a : G) (h : a = a * a) : a = 1 := by
+  calc
+    a = 1 * a := by rw [one_mul]
+    _ = (a ⁻¹ * a) * a := by rw [mul_left_inv]
+    _ = a⁻¹ * (a * a) := by rw [mul_assoc]
+    _ = a⁻¹ * a := by rw [←h]
+    _ = 1 := by rw [mul_left_inv]
+
+namespace MyGroup
+theorem mul_right_inv (a : G) : a * a⁻¹ = 1 := by
+  have h : a * a⁻¹ = (a * a⁻¹) * (a * a⁻¹) := by
+    calc
+      a * a⁻¹ = a * 1 * a⁻¹ := by rw [mul_one]
+      _ = a * (a⁻¹ * a) * a⁻¹ := by rw [←mul_left_inv]
+      _ = (a * a⁻¹) * (a * a⁻¹) := by rw [←mul_assoc, mul_assoc]
+  apply idemp (a * a⁻¹) h
+
+theorem mul_one (a : G) : a * 1 = a := by
+  calc
+    a * 1 = a * (a⁻¹ * a) := by rw [mul_left_inv]
+    _ = (a * a⁻¹) * a := by rw [mul_assoc]
+    _ = 1 * a := by rw [mul_right_inv]
+    _ = a := by rw [one_mul]
+
+theorem mul_cancel_right {a b c : G} (h : a * c = b * c) : a = b := by
+  calc
+    a = a * 1 := by rw [mul_one]
+    _ = a * (c * c⁻¹) := by rw [mul_right_inv]
+    _ = (a * c) * c⁻¹ := by rw [mul_assoc]
+    _ = (b * c) * c⁻¹ := by rw [h]
+    _ = b * (c * c⁻¹) := by rw [←mul_assoc]
+    _ = b * 1 := by rw [mul_right_inv]
+    _ = b := by rw [mul_one]
+
+theorem inv_def (a b : G)(h : b * a = 1) : a⁻¹ = b := by
+  have h' : a⁻¹ * a = b * a  := by rw [h, ←mul_left_inv]
+  apply (mul_cancel_right h')
+
+theorem mul_inv_rev (a b : G) : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by
+  have h : (b⁻¹ * a⁻¹) * (a * b) = 1 := by
+    rw [mul_assoc, ←mul_assoc a⁻¹, mul_left_inv, one_mul, mul_left_inv]
+  apply (inv_def (a * b) (b⁻¹ * a⁻¹)  h)
+
+end MyGroup
+
+end
